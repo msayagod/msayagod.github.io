deb063b3451d2c4143ac865c83882e257e8b2bf4
Mario Sayago <m.sayago@outlook.com>
2025-02-13 02:43:31 +0100

    html and js files fixed

---
+++ js/basic.js
@@ -0,0 +1,9 @@
+// basic.js
+
+// Function to display a greeting message
+function greet(name) {
+    return `Hello, ${name}! Welcome to my portfolio site.`;
+}
+
+// Example usage
+console.log(greet('Visitor'));
