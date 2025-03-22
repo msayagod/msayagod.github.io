5c19b9842d55bd62d167c7127d1e0d0edffd52cb
Mario Sayago <m.sayago@outlook.com>
2025-03-22 02:32:12 +0100

    Simplified animation

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,11 @@ function nextPage(currentPageId, nextPageId) {
     let currentPage = document.getElementById(currentPageId);
     let nextPage = document.getElementById(nextPageId);
 
-    // Aplicar el efecto de rotación a la página actual
-    currentPage.style.transform = 'rotateY(-180deg)';
-
-    // Esperar que termine la animación antes de cambiar la página
-    setTimeout(function() {
-        // Ocultar la página actual y mostrar la siguiente
-        currentPage.style.display = 'none';
-        nextPage.style.display = 'block';
-        nextPage.style.transform = 'rotateY(0deg)';
-    }, 1000); // Tiempo de la animación
+    // Ocultar la página actual y mostrar la siguiente
+    currentPage.style.display = 'none';
+    nextPage.style.display = 'block';
 }
+
+
 
 
