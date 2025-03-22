ecb4256aa29a589ae7eac9091599331543ecf816
Mario Sayago <m.sayago@outlook.com>
2025-03-22 02:17:01 +0100

    Maybe fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
 function nextPage(currentPageId, nextPageId) {
-    // Obtener la página actual y la siguiente
+    // Obtener las páginas actual y siguiente
     let currentPage = document.getElementById(currentPageId);
     let nextPage = document.getElementById(nextPageId);
 
-    // Aplicar la animación de giro a la página actual
+    // Aplicar el efecto de rotación a la página actual
     currentPage.style.transform = 'rotateY(-180deg)';
 
-    // Después de 1 segundo (la duración de la animación), ocultamos la página actual y mostramos la siguiente
+    // Esperar que termine la animación antes de cambiar la página
     setTimeout(function() {
-        currentPage.classList.add('hidden');  // Ocultar la página actual
-        nextPage.classList.remove('hidden'); // Mostrar la siguiente página
-        nextPage.style.transform = 'rotateY(0deg)';  // Ajustar la página siguiente para el efecto de rotación
-    }, 1000); // Tiempo de espera antes de mostrar la siguiente página
+        // Ocultar la página actual y mostrar la siguiente
+        currentPage.style.display = 'none';
+        nextPage.style.display = 'block';
+        nextPage.style.transform = 'rotateY(0deg)';
+    }, 1000); // Tiempo de la animación
 }
+
 
